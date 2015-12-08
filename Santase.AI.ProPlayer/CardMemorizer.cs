24be704e61010a299671243dccfd539fdb32539e
Ivcho <ivo_rankov@mail.bg>
2015-12-08 17:00:10 +0200

    Teamwork code submission

---
+++ Santase.AI.ProPlayer/CardMemorizer.cs
@@ -0,0 +1,269 @@
+﻿namespace Santase.AI.ProPlayer
+{
+	using System;
+	using System.Text;
+	using System.Collections.Generic;
+
+	using Logic.Cards;
+
+    public class CardMemorizer
+    {
+		public const int InitialHandValidSize = 6;
+
+        public static readonly CardType[] AllCardTypes =
+        {
+            CardType.Nine,
+            CardType.Ten,
+            CardType.Jack,
+            CardType.Queen,
+            CardType.King,
+            CardType.Ace
+        };
+
+        public static readonly CardSuit[] AllCardSuits =
+        {
+            CardSuit.Club,
+            CardSuit.Diamond,
+            CardSuit.Heart,
+            CardSuit.Spade
+        };
+
+        public static readonly Tools.CardCollection AllCards = new Tools.CardCollection();
+
+		private Tools.CardCollection undiscoveredCards;
+
+		private Tools.CardCollection myHand;
+
+		private Tools.CardCollection myPlayedCards;
+
+		private Tools.CardCollection opponentPlayedCards;
+
+        static CardMemorizer()
+        {
+            foreach (var cardSuit in CardMemorizer.AllCardSuits)
+            {
+                foreach (var cardType in CardMemorizer.AllCardTypes)
+                {
+                    CardMemorizer.AllCards.Add(new Card(cardSuit, cardType));
+                }
+            }
+        }
+
+		public CardMemorizer(Card trumpCard, ICollection<Card> initialHand)
+		{
+			this.TrumpCard = trumpCard;
+			this.RemainingTrumpCardsCount = 0;
+			this.TrumpCardDrawn = false;
+
+			this.undiscoveredCards = new Tools.CardCollection();
+			this.myHand = new Tools.CardCollection();
+			this.myPlayedCards = new Tools.CardCollection();
+			this.opponentPlayedCards = new Tools.CardCollection();
+
+			foreach (var card in CardMemorizer.AllCards)
+			{
+				if (card.Equals(this.TrumpCard))
+				{
+					continue;
+				}
+
+				if (initialHand.Contains(card))
+				{
+					this.myHand.Add(card);
+				}
+				else
+				{
+					this.undiscoveredCards.Add(card);
+
+					if (card.Suit == this.TrumpCard.Suit)
+					{
+						this.RemainingTrumpCardsCount++;
+					}
+				}
+			}
+
+			if (this.myHand.Count != CardMemorizer.InitialHandValidSize)
+			{
+				throw new ArgumentException($"Initial hand size must be {CardMemorizer.InitialHandValidSize}.");
+			}
+		}
+
+		public Card TrumpCard { get; private set; }
+
+		public Card OldTrumpCard { get; private set; }
+
+		public bool TrumpCardDrawn { get; private set; }
+
+	    public int RemainingTrumpCardsCount { get; private set; }
+
+	    public IReadOnlyCollection<Card> UndiscoveredCards
+	    {
+		    get
+		    {
+			    return this.undiscoveredCards;
+		    }
+	    }
+
+		public IReadOnlyCollection<Card> MyHand
+		{
+			get
+			{
+				return this.myHand;
+			}
+		}
+
+		public IReadOnlyCollection<Card> MyPlayedCards
+	    {
+		    get
+		    {
+			    return this.myPlayedCards;
+		    }
+	    }
+
+	    public IReadOnlyCollection<Card> OpponentPlayedCards
+	    {
+		    get
+		    {
+			    return this.opponentPlayedCards;
+		    }
+	    }
+
+		public Card MyLastPlayedCard { get; set; }
+
+		public ICollection<Card> GetMyHand()
+		{
+			return this.myHand.DeepClone(); 
+		}
+
+		public void LogDrawnCard(Card card)
+		{
+			CardMemorizer.ValidateCardNotNull(card);
+
+			if (card.Equals(this.TrumpCard))
+			{
+				this.TrumpCardDrawn = true;
+			}
+			else
+			{
+				this.NewCardDicovered(card);
+			}
+
+			this.myHand.Add(card);
+		}
+
+	    public void LogPlayedCard(Card card)
+	    {
+			CardMemorizer.ValidateCardNotNull(card);
+
+			bool removed = this.myHand.Remove(card);
+
+			if (!removed)
+			{
+				throw new ArgumentException($"Card must be present in {nameof(this.MyHand)}");
+			}
+
+			this.MyLastPlayedCard = card;
+
+		    this.myPlayedCards.Add(card);
+	    }
+
+	    public void LogOpponentPlayedCard(Card card)
+	    {
+			CardMemorizer.ValidateCardNotNull(card);
+
+			bool isTrumpCard = card.Equals(this.TrumpCard);
+
+			if (!isTrumpCard && !card.Equals(this.OldTrumpCard))
+			{
+				this.NewCardDicovered(card);
+			}
+
+			if (isTrumpCard)
+			{
+				this.TrumpCardDrawn = true;
+			}
+
+			this.opponentPlayedCards.Add(card);
+	    }
+
+	    public void LogTrumpChange()
+	    {
+			Card lowestTrump = new Card(this.TrumpCard.Suit, CardType.Nine);
+
+			if (this.myHand.Contains(lowestTrump))
+			{
+				this.myHand.Remove(lowestTrump);
+				this.myHand.Add(this.TrumpCard);
+			}
+			else if (this.undiscoveredCards.Contains(lowestTrump))
+			{
+				this.NewCardDicovered(lowestTrump);
+			}
+			else
+			{
+				throw new ArgumentException("The lowest trump card has alredy been played.");
+			}
+
+			this.OldTrumpCard = this.TrumpCard;
+			this.TrumpCard = lowestTrump;
+	    }
+
+	    public void LogTrumpCardDrawn()
+	    {
+		    this.TrumpCardDrawn = true;
+	    }
+
+		private static void ValidateCardNotNull(Card card)
+		{
+			if (card == null)
+			{
+				throw new ArgumentNullException("Can't log null card.");
+			}
+		}
+
+		private void NewCardDicovered(Card card)
+	    {
+			bool removed = this.undiscoveredCards.Remove(card);
+
+			if (!removed)
+			{
+				throw new ArgumentException($"Card Must be present in {nameof(this.UndiscoveredCards)}.");
+			}
+
+			if (card.Suit == this.TrumpCard.Suit)
+			{
+				this.RemainingTrumpCardsCount--;
+			}
+		}
+
+		public override string ToString()
+		{
+			StringBuilder result = new StringBuilder();
+
+			const string Separator = ": ";
+			const string ListingSeparator = ", ";
+
+
+			result.AppendLine(nameof(this.UndiscoveredCards) + Separator);
+			result.AppendLine(string.Join(ListingSeparator, this.UndiscoveredCards));
+
+			result.AppendLine(nameof(this.MyHand) + Separator);
+			result.AppendLine(string.Join(ListingSeparator, this.MyHand));
+
+			result.AppendLine(nameof(this.MyPlayedCards) + Separator);
+			result.AppendLine(string.Join(ListingSeparator, this.MyPlayedCards));
+
+			result.AppendLine(nameof(this.OpponentPlayedCards) + Separator);
+			result.AppendLine(string.Join(ListingSeparator, this.OpponentPlayedCards));
+
+			result.AppendLine(nameof(this.MyLastPlayedCard) + Separator + this.MyLastPlayedCard);
+
+			result.AppendLine(nameof(this.TrumpCard) + Separator + this.TrumpCard);
+			result.AppendLine(nameof(this.OldTrumpCard) + Separator + this.OldTrumpCard);
+			result.AppendLine(nameof(this.TrumpCardDrawn) + Separator + this.TrumpCardDrawn);
+			result.AppendLine(nameof(this.RemainingTrumpCardsCount) + Separator + this.RemainingTrumpCardsCount);
+
+			return result.ToString();
+		}
+	}
+}
---
+++ Santase.AI.ProPlayer/ProPlayer.cs
@@ -0,0 +1,314 @@
+﻿namespace Santase.AI.ProPlayer
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Logic;
+    using Logic.Cards;
+    using Logic.PlayerActionValidate;
+    using Logic.Players;
+	using Logic.WinnerLogic;
+
+	using States;
+	using Tools.Extensions;
+
+	public class ProPlayer : IPlayer
+	{
+		public ProPlayer()
+		{
+			this.AnnounceValidator = new AnnounceValidator();
+			this.PlayerActionValidator = new PlayerActionValidator();
+			this.CardWinnerLogic = new CardWinnerLogic();
+		}
+
+		private IState State { get; set; }
+
+		public bool IsFirstPlayer { get; set; }
+
+		public int MyPoints { get; private set; }
+
+		public int OpponentPoints { get; private set; }
+
+		public CardMemorizer CardMemorizer { get; private set; }
+
+		public IReadOnlyCollection<Card> MyHand
+		{
+			get
+			{
+				return this.CardMemorizer.MyHand;
+			}
+		}
+
+		public IAnnounceValidator AnnounceValidator { get; }
+
+		public IPlayerActionValidator PlayerActionValidator { get; }
+
+		public ICardWinnerLogic CardWinnerLogic { get; }
+
+		public string Name => "Potato!";
+
+		public virtual void StartGame(string otherPlayerIdentifier)
+		{
+		}
+
+		public virtual void StartRound(ICollection<Card> cards, Card trumpCard, int myTotalPoints, int opponentTotalPoints)
+		{
+			this.CardMemorizer = new CardMemorizer(trumpCard, cards);
+		}
+
+		public virtual void AddCard(Card card)
+		{
+			this.CardMemorizer.LogDrawnCard(card);
+		}
+
+		public virtual PlayerAction GetTurn(PlayerTurnContext context)
+		{
+			this.IsFirstPlayer = context.IsFirstPlayerTurn;
+
+			if (context.CardsLeftInDeck == 0)
+			{
+				this.CardMemorizer.LogTrumpCardDrawn();
+			}
+
+			this.SetCorrectState(context);
+
+			var myHand = this.CardMemorizer.GetMyHand();
+			if (this.PlayerActionValidator.IsValid(PlayerAction.ChangeTrump(), context, myHand))
+			{
+				return this.ChangeTrump();
+			}
+
+			if (this.ShouldCloseGame(context))
+			{
+				return this.CloseGame();
+			}
+
+			return this.ChooseCard(context);
+		}
+
+		public virtual void EndTurn(PlayerTurnContext context)
+		{
+			if (!this.CardMemorizer.TrumpCardDrawn && !this.CardMemorizer.TrumpCard.Equals(context.TrumpCard))
+			{
+				this.CardMemorizer.LogTrumpChange();
+			}
+
+			if (this.IsFirstPlayer)
+			{
+				this.MyPoints = context.FirstPlayerRoundPoints;
+				this.OpponentPoints = context.SecondPlayerRoundPoints;
+			}
+			else
+			{
+				this.MyPoints = context.SecondPlayerRoundPoints;
+				this.OpponentPoints = context.FirstPlayerRoundPoints;
+			}
+
+			Card opponentCard = this.GetOpponentCard(context);
+
+			if (opponentCard != null)
+			{
+				this.CardMemorizer.LogOpponentPlayedCard(opponentCard);
+			}
+		}
+
+		public virtual void EndRound()
+		{
+			;
+		}
+
+		public virtual void EndGame(bool amIWinner)
+		{
+			;
+		}
+
+		public PlayerAction PlayCard(Card card)
+		{
+			this.CardMemorizer.LogPlayedCard(card);
+			return PlayerAction.PlayCard(card);
+		}
+
+		public int CalcHandValue()
+		{
+			return this.MyHand.Sum(card => card.GetValue());
+		}
+
+		public Card GetOpponentCard(PlayerTurnContext context)
+		{
+			return this.IsFirstPlayer ? context.SecondPlayedCard : context.FirstPlayedCard;
+		}
+
+		public Card GetPossibleBestAnounce(ICollection<Card> possibleCardsToPlay)
+
+		{
+			foreach (var card in possibleCardsToPlay)
+			{
+				if (!this.IsTrumpCard(card))
+				{
+					continue;
+				}
+
+				if (card.Type == CardType.King && possibleCardsToPlay.Any(c => c.Suit == card.Suit && c.Type == CardType.Queen))
+				{
+					return card;
+				}
+			}
+
+			foreach (var card in possibleCardsToPlay)
+			{
+				if (card.Type == CardType.King && possibleCardsToPlay.Any(c => c.Suit == card.Suit && c.Type == CardType.Queen))
+				{
+					return card;
+				}
+			}
+
+			return null;
+		}
+
+		public IEnumerable<Card> GetWinningCardsWhenFirst(IEnumerable<Card> cards)
+		{
+			var opponentCards = this.GetPossibleOpponentCards();
+			var winningCards = cards.ToList();
+			foreach (var card in cards)
+			{
+				foreach (var oponentCard in opponentCards)
+				{
+					if (oponentCard.Suit == card.Suit)
+					{
+						if (oponentCard.GetValue() > card.GetValue())
+						{
+							winningCards.Remove(card);
+						}
+					}
+				}
+			}
+
+			return winningCards;
+		}
+
+		public IEnumerable<Card> GetWinningCardsWhenSecond(IEnumerable<Card> cards, Card cardToBeat)
+		{
+			return cards
+				.Where(card => this.CardWinnerLogic.Winner(cardToBeat, card, this.CardMemorizer.TrumpCard.Suit) == PlayerPosition.SecondPlayer)
+				.OrderBy(card => card.GetValue());
+		}
+
+		public int CalcPossibleRemainingTurns(bool deckIsClosed)
+		{
+			int cardsToBePlayedCount = this.CardMemorizer.MyHand.Count;
+
+			if (deckIsClosed)
+			{
+				return cardsToBePlayedCount;
+			}
+
+			cardsToBePlayedCount += this.CardMemorizer.UndiscoveredCards.Count;
+
+			if (!this.CardMemorizer.TrumpCardDrawn)
+			{
+				cardsToBePlayedCount++;
+			}
+
+			return cardsToBePlayedCount / 2;
+		}
+
+		public Card GetWeakestCard(IEnumerable<Card> cards)
+		{
+			Card weakestCard = cards.First();
+
+			foreach (var card in cards)
+			{
+				if ((this.IsTrumpCard(weakestCard) || !this.IsTrumpCard(card)) && (card.GetValue() < weakestCard.GetValue()))
+				{
+					weakestCard = card;
+				}
+			}
+
+			return weakestCard;
+		}
+
+		public bool IsTrumpCard(Card card)
+		{
+			return card.Suit == this.CardMemorizer.TrumpCard.Suit;
+		}
+
+		public int CountPotentialWins(Card card)
+		{
+			int count = 0;
+
+			var opponentCards = this.GetPossibleOpponentCards();
+
+			foreach (var opponentCard in opponentCards)
+			{
+				if (this.CardWinnerLogic.Winner(card, opponentCard, this.CardMemorizer.TrumpCard.Suit) == PlayerPosition.FirstPlayer)
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
+
+		public IReadOnlyCollection<Card> GetPossibleOpponentCards()
+		{
+			if (this.CardMemorizer.OldTrumpCard == null)
+			{
+				return this.CardMemorizer.UndiscoveredCards;
+			}
+
+			var result = new Tools.CardCollection();
+
+			this.CardMemorizer.UndiscoveredCards.ForEach(result.Add);
+
+			result.Add(this.CardMemorizer.OldTrumpCard);
+
+			return result;
+		}
+
+		protected PlayerAction ChooseCard(PlayerTurnContext context)
+		{
+			return this.State.ChooseCard(context);
+		}
+
+		protected PlayerAction ChangeTrump()
+		{
+			this.CardMemorizer.LogTrumpChange();
+			return PlayerAction.ChangeTrump();
+		}
+
+		protected PlayerAction CloseGame()
+		{
+			return PlayerAction.CloseGame();
+		}
+
+		protected virtual bool ShouldCloseGame(PlayerTurnContext context)
+		{
+			bool canCloseGame = this.PlayerActionValidator.IsValid(PlayerAction.CloseGame(), context, this.CardMemorizer.GetMyHand());
+
+			if (canCloseGame)
+			{
+				int potentialPoints = this.MyPoints + this.CalcHandValue();
+				var myThrumpCardCount = this.CardMemorizer.GetMyHand().Count(x => x.Suit == context.TrumpCard.Suit);
+				return potentialPoints >= 60 && myThrumpCardCount >= 3;
+			}
+
+			return false;
+		}
+
+		protected virtual void SetCorrectState(PlayerTurnContext context)
+		{
+			if (!this.IsFirstPlayer)
+			{
+				this.State = new PlayingSecond(this);
+			}
+			else if (context.State.ShouldObserveRules)
+			{
+				this.State = new PlayingFirstWithRules(this);
+			}
+			else
+			{
+				this.State = new PlayingFirst(this);
+			}
+		}
+	}
+}
